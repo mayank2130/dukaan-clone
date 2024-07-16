4d3a63647bd69cfddbcb0dd5caee5d9bcd87c2cc
mayank2130 <mayankthakur1712@gmail.com>
2024-07-16 20:46:37 +0530

    returns

--- src/components/Returns.tsx
+++ src/components/Returns.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 const Returns = () => {
   return (
-    <div>Returns</div>
-  )
-}
+    <div className="bg-white shadow rounded-lg w-full h-96 flex flex-col justify-end">
+      <div className="px-4 sm:p-6">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center space-y-2 mb-8">
+            <Image
+              src="/assets/images.png"
+              alt="return_logo"
+              width={120}
+              height={120}
+            />
+            <p className="font-medium">Set up Returns & Replacement</p>
+            <p className="text-sm text-slate-500">
+              Set up returns and replacement for your delivered orders and
+              manage them easily from your Dukaan dashboard.
+            </p>
+          </div>
+          <Button>Set up</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Returns
+export default Returns;
